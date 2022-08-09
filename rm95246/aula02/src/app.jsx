e1b2fef6e0987b761cb2c71b8d4cb8576c227c22
btwbeatriz <rm95246@fiap.com.br>
2022-08-09 09:48:26 -0300

    criado componentes e listas

---
+++ rm95246/aula02/src/app.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ListaAlunos from './components/ListaAlunos/ListaAlunos'
+
+const App = () => {
+
+    const aluno = 'Igor Ramalho'
+    const curso = 'ADS'
+    const alunos = ['Alexandre', 'Ronqui', 'Fernando']
+
+    const novoAluno = () => 'Ronqui'
+
+    return(
+        <div>
+            <h1>Trabalhando com PROPS</h1>
+            <p>Aluno: {aluno}</p>
+            <p>Curso: {curso}</p>
+            <ListaAlunos alunos={alunos}
+            maisAluno={novoAluno} />
+        </div>
+    )
+}
+
+export default App
