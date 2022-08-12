d7b23d21eccd84db662d0a7efd2b0f30e4d17c6d
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-12 11:20:01 -0300

    :sparkles: aula02 props

--- rm93693/aula02/src/App.jsx
+++ rm93693/aula02/src/App.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import ListaAlunos from "./components/ListaAlunos/ListaAlunos";
+import "./App.css";
 
 const App = () => {
   const aluno = "Paulo Henrique";
   const curso = "ADS";
-  const alunos = ["Alexandre", "Ronqui", "Guilherme"];
-
-  const novoAluno = () => "Ronqui";
+  const alunos = ["Alexandre", "Ronqui", "Guilherme", "Fernando"];
 
   return (
     <>
       <h1>Trabalhando com PROPS</h1>
       <p>Aluno: {aluno}</p>
       <p>Curso: {curso}</p>
-      <ListaAlunos alunos={alunos} maisAluno={novoAluno} />
+      <ListaAlunos alunos={alunos}/>
     </>
   );
 };
--- rm93693/aula02/src/components/ListaAlunos/ListaAlunos.jsx
+++ rm93693/aula02/src/components/ListaAlunos/ListaAlunos.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./ListaAlunos.css";
 
 export default function ListaAlunos(props) {
   return (
-    <ul>
-      <li>{props.alunos[0]}</li>
-      <li>{props.alunos[1]}</li>
-      <li>{props.alunos[2]}</li>
-      <li>{props.maisAluno()}</li>
+    <ul className="listaAluno">
+      {
+      props.alunos.map((aluno, i) =>
+      <li key={i}>{aluno}</li>
+      )}
     </ul>
   );
 }
