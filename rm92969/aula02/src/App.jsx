02d06b7f23bbea0b334675dfb42162dd2aa8bc26
Vinícius Yoda <vyoda4604@gmail.com>
2022-08-12 11:16:28 -0300

    props, passagem de pai para filho, map

--- rm92969/aula02/src/App.jsx
+++ rm92969/aula02/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ListaAlunos from './components/ListaAlunos/ListaAlunos'
+import './App.css'
 
 const App = () => {
 
     const aluno = 'Vinícius Yoda'
     const curso = 'ADS'
-    const alunos = ['Arvelan', 'Igor', 'Raphael']
+    const alunos = ['Arvelan', 'Igor', 'Raphael', 'Fernando']
 
     const novoAluno = () => 'Mario'
 
@@ -14,7 +15,7 @@ const App = () => {
             <h1>Trabalhando com props</h1>
             <p>Aluno : {aluno}</p>
             <p>Curso : {curso}</p>
-            <ListaAlunos alunos={alunos} maisAluno={novoAluno} />
+            <ListaAlunos alunos={alunos}  />
         </div>
     )
 }
