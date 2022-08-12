32e876014733090b0bac049ba2ba3efc3c2b7809
igorcamargo1 <rm94553@fiap.com.br>
2022-08-12 11:16:44 -0300

    resolução exercicios props

--- rm94553/aula02/src/App.jsx
+++ rm94553/aula02/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import ListaAlunos from "./components/ListaAlunos/ListaAlunos"
+import './App.css'
 
 const App = () => {
     
     const aluno = 'Igor Gabriel'
     const curso = 'ADS'
-    const alunos = ['Alexandre', 'Ronqui', 'Guilherme']
+    const alunos = ['Alexandre', 'Ronqui', 'Guilherme','Fernando']
     
     const novoAluno = () => 'Ronqui'
 
@@ -14,8 +15,9 @@ const App = () => {
             <h1>Trabalhando com PROPS</h1>
             <p>Aluno: {aluno}</p>
             <p>Curso: {curso}</p>
-            <ListaAlunos alunos={alunos}
-            maisAlunos={novoAluno}/>
+            <ListaAlunos 
+            alunos={alunos}
+            />
         </div>
     )
 }
